a8acf9292dc8e7382c6850a8d9134105c058b24c
Jhennys <j.andreza26@gmail.com>
2023-05-02 21:40:14 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'dart:async' show Timer;
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+// Checking
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _seconds = 00;
+  int _minutes = 25;
+  Timer? _timer;
+  var f = NumberFormat("00");
+
+  void _stopTimer() {
+    if (_timer != null) {
+      _timer?.cancel();
+      _seconds = 0;
+      _minutes = 25;
+    }
+  }
+
+  void _startTimer() {
+    if (_timer != null) {
+      _stopTimer();
+    }
+    if (_minutes > 0) {
+      _seconds = _minutes * 60;
+    }
+    if (_seconds > 60) {
+      _minutes = (_seconds / 60).floor();
+      _seconds -= (_minutes * 60);
+    }
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_seconds > 0) {
+          _seconds--;
+        } else {
+          if (_minutes > 0) {
+            _seconds = 59;
+            _minutes--;
+          } else {
+            _timer?.cancel();
+            print("Timer Complete");
+          }
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${f.format(_minutes)} : ${f.format(_seconds)}",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 300,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _stopTimer();
+                  });
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: const CircleBorder(
+                      side: BorderSide(color: Colors.orange)),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(40.0),
+                  child: Text(
+                    "Stop",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _startTimer();
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  shape: const CircleBorder(
+                      side: BorderSide(color: Colors.orange)),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(40.0),
+                  child: Text(
+                    "Start",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
